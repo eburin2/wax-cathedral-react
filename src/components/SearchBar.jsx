c8c169c3068ed7dbafb5a003cf7e62d8626260b2
Guest User <Guest@Epicodus-2A.local>
2018-02-01 11:39:25 -0800

    add searchbar functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import { v4 } from 'uuid';
 
-
 function SearchBar(props){
   let _origin = null;
-  console.log(origin);
 
   function handleSearchFormSubmission(event) {
+    event.preventDefault();
+    console.log(_origin.value);
+    _origin.value = '';
   }
 
+  console.log('yo');
 
 
 
